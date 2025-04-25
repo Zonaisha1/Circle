5663dab505cd249cf120a1167869ef0191f02bad
Zonaisha Fatima <fatimazonaisha@gmail.com>
2025-04-25 12:51:20 +0500

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,9 @@
+let rad = parseInt(prompt("Enter Radius of a circle"));
+console.log('Radius = '+rad);
+
+ const pie = 3.14;
+ let diameter = 2 * rad;
+ let circum = 2 * pie * rad;
+ let area = pie * rad * rad;
+ console.log('The Diameter of a Circle is : '+diameter+'\nThe Circumference of a Circle is : '+circum+'\nThe Area of a Circle is : '+area);
+ 
